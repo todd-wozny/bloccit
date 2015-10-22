6041f20c8072515db50f68e3c9ec4315ff89f91c
Todd Wozny <toddwozny@gmail.com>
2015-10-22 22:13:03 +0000

    Assignment 30 completed!

--- bloccit/db/seeds.rb
+++ bloccit/db/seeds.rb
@@ -21,6 +21,10 @@ include RandomData
    )
  end
  
+ puts "#{Post.count}"
+ Post.find_or_create_by(title: "A unique title", body: "A unique body")
+ puts "#{Post.count}"
+ 
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"

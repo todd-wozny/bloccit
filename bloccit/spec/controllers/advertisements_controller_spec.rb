d4e765bb1dddc145fd6243b25ab5800ba262d232
Todd Wozny <toddwozny@gmail.com>
2015-10-28 17:08:24 +0000

    Work in progress

---
+++ bloccit/spec/controllers/advertisements_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+include RandomData
+
+RSpec.describe AdvertisementsController, type: :controller do
+  let (:my_ad) do
+    Advertisement.create(
+      id: 1,
+      title: RandomData.random_sentence,
+      copy:  RandomData.random_paragraph,
+      price: 99
+      )
+    end
+
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+    
+    it "assigns [my_ad] to @advertisements" do 
+      get :index
+      expect(assigns(:advertisements)).to eq([my_ad])
+      end      
+    end
+
+  
+
+end

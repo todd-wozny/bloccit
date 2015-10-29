97768d6ab7461fba27cd56509bf3ce832c21d492
Todd Wozny <toddwozny@gmail.com>
2015-10-29 16:46:52 +0000

    Assignment 32 almost complete

--- bloccit/spec/controllers/advertisements_controller_spec.rb
+++ bloccit/spec/controllers/advertisements_controller_spec.rb
@@ -22,7 +22,42 @@ RSpec.describe AdvertisementsController, type: :controller do
       expect(assigns(:advertisements)).to eq([my_ad])
       end      
     end
-
+  
+  describe "GET #show" do
+    it "returns http success" do
+      get :show, {id: my_ad.id}
+      expect(response).to have_http_status(:success)
+    end
+    
+    it "renders the #show view" do 
+      get :show, {id: my_ad.id}
+      expect(response).to render_template :show
+    end
+    
+    it "assigns my_ad to @advertisement" do 
+      get :show, {id: my_ad.id}
+      expect(assigns(:advertisement)).to eq(my_ad)
+      end      
+    end
+    #Create Post
+    describe "POST create" do
+    it "increases the number of advertisements by 1" do
+      expect{post :create, advertisement: {title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: 99}}.to  
+      change(Advertisement,:count).by(1)
+    end
+    
+    it "assigns the new advertisement to @advertisement" do 
+      post :create, advertisement: {title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: 99}
+      expect(assigns(:advertisement)).to eq Advertisement.last
+    end
+    
+    it "it redirects to the new advertisement" do 
+      post :create, advertisement: {title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: 99}
+      expect(response).to redirect_to Advertisement.last
+      end      
+    end
+    
+    
   
 
 end

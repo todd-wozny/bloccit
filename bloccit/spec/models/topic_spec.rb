3fdc8e7139dd1c36845f0bfb841b2964c9f87fac
Todd Wozny <toddwozny@gmail.com>
2015-11-11 22:34:43 +0000

    Assignment 35 completed, (sort of)

--- bloccit/spec/models/topic_spec.rb
+++ bloccit/spec/models/topic_spec.rb
@@ -6,6 +6,13 @@ RSpec.describe Topic, type: :model do
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
   
   it { should have_many(:posts) }
+  
+   it { should validate_presence_of(:name) }
+   it { should validate_presence_of(:description) }
+  
+ # 2
+   it { should validate_length_of(:name).is_at_least(5) }
+   it { should validate_length_of(:description).is_at_least(15) }
  
  # #1
    describe "attributes" do
@@ -25,6 +32,8 @@ RSpec.describe Topic, type: :model do
      it "should be public by default" do
        expect(topic.public).to be(true)
      end
+  
    end
+ 
  end
  

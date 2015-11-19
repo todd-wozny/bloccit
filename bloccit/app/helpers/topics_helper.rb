d255ffd898d51a0f29da20b2381073f5c951a15b
Alex Spencer <alex@alexspencer.net>
2015-11-19 18:51:09 -0500

    updated code from mentor call

--- bloccit/app/helpers/topics_helper.rb
+++ bloccit/app/helpers/topics_helper.rb
@@ -1,5 +1,9 @@
 module TopicsHelper
     def user_is_authorized_for_topics?
-        current_user && current_user.admin?
-   end
+      current_user && (current_user.admin? || current_user.moderator?)
+    end
+
+    def user_is_authorized_for_posts?(post)
+      current_user && (current_user.admin? || current_user.moderator? || current_user == post.user)
+    end
 end
--- bloccit/app/models/user.rb
+++ bloccit/app/models/user.rb
@@ -1,25 +1,19 @@
 class User < ActiveRecord::Base
-    has_many :posts
-    before_save { self.email = email.downcase }
-    before_save { self.role ||= :member }
+  has_many :posts
+  before_save { self.email = email.downcase }
+  before_save { self.role ||= :member }
 
- # #3
-   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
- # #4
-   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
- # #5
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
   validates :password, length: { minimum: 6 }, allow_blank: true
- # #6
-   validates :email,
+  validates :email,
              presence: true,
              uniqueness: { case_sensitive: false },
              length: { minimum: 3, maximum: 100 },
              format: { with: EMAIL_REGEX }
 
- # #7
-   has_secure_password
-   
-   enum role: [:member, :admin]
+  has_secure_password
+
+  enum role: [:member, :admin, :moderator]
 end

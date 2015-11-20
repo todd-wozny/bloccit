f27211c722425cd2a62ef1caeb3a8644361ede3f
Todd Wozny <toddwozny@gmail.com>
2015-11-20 20:55:51 +0000

    Sending user.rb to master

--- bloccit/app/models/user.rb
+++ bloccit/app/models/user.rb
@@ -3,23 +3,23 @@ class User < ActiveRecord::Base
     before_save { self.email = email.downcase }
     before_save { self.role ||= :member }
 
- # #3
+ 
    EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
- # #4
+ 
    validates :name, length: { minimum: 1, maximum: 100 }, presence: true
- # #5
+ 
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
   validates :password, length: { minimum: 6 }, allow_blank: true
- # #6
+ 
    validates :email,
              presence: true,
              uniqueness: { case_sensitive: false },
              length: { minimum: 3, maximum: 100 },
              format: { with: EMAIL_REGEX }
 
- # #7
+ 
    has_secure_password
    
-   enum role: [:member, :admin]
+   enum role: [:member, :admin, :moderator]
 end

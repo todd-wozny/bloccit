8dfd766c1f17a8ebe01daf4120acca49323055fb
Todd Wozny <toddwozny@gmail.com>
2015-12-11 19:15:28 +0000

    Checkpoint 43 is complete

--- bloccit/db/schema.rb
+++ bloccit/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151208195345) do
+ActiveRecord::Schema.define(version: 20151211185217) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20151208195345) do
     t.integer  "topic_id"
     t.integer  "user_id"
     t.integer  "rating_id"
+    t.float    "rank"
   end
 
   add_index "posts", ["rating_id"], name: "index_posts_on_rating_id"
@@ -124,5 +125,16 @@ ActiveRecord::Schema.define(version: 20151208195345) do
     t.datetime "updated_at",      null: false
     t.integer  "role"
   end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "value"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "votes", ["post_id"], name: "index_votes_on_post_id"
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id"
 
 end
--- bloccit/db/seeds.rb
+++ bloccit/db/seeds.rb
@@ -23,12 +23,17 @@ include RandomData
  # Create Posts
  50.times do
  
-   Post.create!(
+   post = Post.create!(
      user:   users.sample,
      topic:  topics.sample,
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
    )
+   
+   post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+ 
+   rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
+   
  end
  posts = Post.all
 
@@ -65,8 +70,7 @@ include RandomData
    password: 'helloworld'
  )
 
- puts "Seed finished"
- puts "#{User.count} users created"
  puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
+ puts "#{Vote.count} votes created"

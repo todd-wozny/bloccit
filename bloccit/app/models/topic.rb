032da9cf8ecb7e44f7083ec4493197462eab102f
Todd Wozny <toddwozny@gmail.com>
2015-12-21 03:10:31 +0000

    Assignment 46 complete

--- bloccit/app/models/topic.rb
+++ bloccit/app/models/topic.rb
@@ -4,5 +4,7 @@ class Topic < ActiveRecord::Base
    has_many :labels, through: :labelings
    belongs_to :rating
    
-   scope :visible_to, -> (user) { user ? all : where(public: true) }
+   scope :publically_viewable, -> { where(public: true) }
+   scope :privately_viewable, -> { where(public: false) }
+   scope :visible_to, -> (user) { user ? all : publically_viewable }
 end

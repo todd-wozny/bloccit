e548fb600c7bb1ff3f734684ad64ff83359dc55a
Todd Wozny <toddwozny@gmail.com>
2016-01-03 19:33:20 +0000

    Checkpoint 48 APIRetrieve Data: completed

--- bloccit/config/routes.rb
+++ bloccit/config/routes.rb
@@ -25,12 +25,15 @@ Rails.application.routes.draw do
     get 'welcome/contacts'
   
     get 'welcome/faq'
-    
-    
-
-
  
-     root to: 'welcome#index'
+    root to: 'welcome#index'
+    
+    namespace :api do
+     namespace :v1 do
+       resources :users, only: [:index, :show]
+       resources :topics, only: [:index, :show]
+     end
+    end
 
  
 end
--- bloccit/db/schema.rb
+++ bloccit/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151213161233) do
+ActiveRecord::Schema.define(version: 20160103042237) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -134,6 +134,7 @@ ActiveRecord::Schema.define(version: 20151213161233) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "role"
+    t.string   "auth_token"
   end
 
   create_table "votes", force: :cascade do |t|
